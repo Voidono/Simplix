1259d9acb4284ed96b396c93c90696c83cdb6658
manh1122005 <voidoistaken@gmail.com>
2025-07-11 16:41:29 +0700

    config change

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
-import {NextConfig} from 'next';
+import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
-
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
-const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
