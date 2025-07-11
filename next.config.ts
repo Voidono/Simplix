bb95fef2888330fef27e1e4cdd94888e6fa5d5c1
manh1122005 <voidoistaken@gmail.com>
2025-07-11 16:24:45 +0700

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = {};
- 
-const withNextIntl = createNextIntlPlugin();
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 export default withNextIntl(nextConfig);
